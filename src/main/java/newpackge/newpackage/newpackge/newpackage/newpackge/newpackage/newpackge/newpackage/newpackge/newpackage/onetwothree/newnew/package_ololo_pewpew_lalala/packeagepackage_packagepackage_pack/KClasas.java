65c5940e30616eb66ab5ff3debd7bec1dd1fa3c7
viktoria.bozhko <viktoria.bozhko@jetbrains.com>
2021-03-23 16:10:39 +0300

    pom

---
+++ src/main/java/newpackge/newpackage/newpackge/newpackage/newpackge/newpackage/newpackge/newpackage/newpackge/newpackage/onetwothree/newnew/package_ololo_pewpew_lalala/packeagepackage_packagepackage_pack/KClasas.java
@@ -0,0 +1,4 @@
+package newpackge.newpackage.newpackge.newpackage.newpackge.newpackage.newpackge.newpackage.newpackge.newpackage.onetwothree.newnew.package_ololo_pewpew_lalala.packeagepackage_packagepackage_pack;
+
+public class KClasas {
+}
---
+++ src/test/java/newpackge/newpackage/newpackge/newpackage/newpackge/newpackage/newpackge/newpackage/newpackge/newpackage/onetwothree/newnew/package_ololo_pewpew_lalala/lkkljkihkjgjhghvf/gffhgguygyghf/TestnameTestnameTestnameTestnameTestnameTestnameTestnameTestname.java
@@ -0,0 +1,15 @@
+package newpackge.newpackage.newpackge.newpackage.newpackge.newpackage.newpackge.newpackage.newpackge.newpackage.onetwothree.newnew.package_ololo_pewpew_lalala.lkkljkihkjgjhghvf.gffhgguygyghf;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestnameTestnameTestnameTestnameTestnameTestnameTestnameTestname {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+
+}
